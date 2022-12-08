0bb773d02d7464f4e8c6d1815a59e41d8450b32e
levin-achieng-dieto <levin.dieto@student.moringaschool.com>
2022-12-08 21:34:50 +0300

    seeding house table

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
 House.destroy_all
 
-house1 = House.create(name: "Bungalow", image_url: "https://i.postimg.cc/cLb52STf/1.jpg", price: 20,000,000)
-house2 = House.create(name: "Mill House", image_url: "https://i.postimg.cc/htDf6Fh4/Cottage-Design.jpg", price: 35,000,000)
-house3 = House.create(name: "Orchad House", image_url: "https://i.postimg.cc/NGdVmmTN/Dover-Pool-House-Patrick-Ahearn-Architect.jpg", price: 120,000,000)
-house4 = House.create(name: "The Laurels", image_url: "https://i.postimg.cc/gc17gdSR/Southampton-Home-Ashley-Darryl-Interiors.jpg", price: 50,000,000)
+house1 = House.create(name: "Bungalow", image_url: "https://i.postimg.cc/cLb52STf/1.jpg", price: 20000000)
+house2 = House.create(name: "Mill House", image_url: "https://i.postimg.cc/htDf6Fh4/Cottage-Design.jpg", price: 35000000)
+house3 = House.create(name: "Orchad House", image_url: "https://i.postimg.cc/NGdVmmTN/Dover-Pool-House-Patrick-Ahearn-Architect.jpg", price: 120000000)
+house4 = House.create(name: "The Laurels", image_url: "https://i.postimg.cc/gc17gdSR/Southampton-Home-Ashley-Darryl-Interiors.jpg", price: 50000000)
