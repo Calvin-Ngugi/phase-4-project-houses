106762a54e21aae4a249b005538cfce5bbd00495
levin-achieng-dieto <levin.dieto@student.moringaschool.com>
2022-12-08 21:31:38 +0300

    adding seeds to seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,6 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+House.destroy_all
+
+house1 = House.create(name: "Bungalow", image_url: "https://i.postimg.cc/cLb52STf/1.jpg", price: 20,000,000)
+house2 = House.create(name: "Mill House", image_url: "https://i.postimg.cc/htDf6Fh4/Cottage-Design.jpg", price: 35,000,000)
+house3 = House.create(name: "Orchad House", image_url: "https://i.postimg.cc/NGdVmmTN/Dover-Pool-House-Patrick-Ahearn-Architect.jpg", price: 120,000,000)
+house4 = House.create(name: "The Laurels", image_url: "https://i.postimg.cc/gc17gdSR/Southampton-Home-Ashley-Darryl-Interiors.jpg", price: 50,000,000)
