cad8a730d5457f0a886262fc875bdc23562330ba
levin-achieng-dieto <levin.dieto@student.moringaschool.com>
2022-12-09 13:25:18 +0300

    styling home page

--- client/src/Components/Home.js
+++ client/src/Components/Home.js
@@ -17,8 +17,10 @@ function Home({ user }) {
     return (
       <>
         <div className="container">
-          <h2>Welcome, {user.username}, and find</h2>
-          <h2>yourself a house to build a home</h2>
+          <div className="w-20 bg-black text-white text-center">
+            <h2 className="pt-4">Welcome, {user.username}, and find</h2>
+            <h2 className="pb-4">yourself a house to build a home</h2>
+          </div>
           <Houses house={house}/>
         </div>
       </>
