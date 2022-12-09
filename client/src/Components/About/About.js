e60d2e2d4653183fd410a63358ace728a53cf0bb
Bodipoh <berylodipoh@gmail.com>
2022-12-09 13:27:59 +0300

    About added

---
+++ client/src/Components/About/About.js
@@ -0,0 +1,25 @@
+import React from "react"
+
+const About = () => {
+  return (
+    <>
+      <section className='about'>
+        <div name='About Us' title='About Us - Who We Are?' cover={img} />
+        <div className='container flex mtop'>
+          <div className='left row'>
+            <h1>Our Agency Story</h1>
+            <h2>Check out our company story and work process</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip.</p>
+            <button className='btn2'>More About Us</button>
+          </div>
+          <div className='right row'>
+            <img src='./immio.jpg' alt='' />
+          </div>
+        </div>
+      </section>
+    </>
+  )
+}
+
+export default About
