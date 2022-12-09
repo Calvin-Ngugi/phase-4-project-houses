ea5f246948dee2542a9e2b14d2dd6dd295220970
calvin <ncalvin67@gmail.com>
2022-12-09 11:19:02 +0300

    Fetching list of houses

--- client/src/Components/Home.js
+++ client/src/Components/Home.js
@@ -1,17 +1,31 @@
+import { useEffect, useState } from "react";
+import Houses from "./Houses/Houses";
+
 function Home({ user }) {
-    if (user) {
-      return (
+  const [house, setHouses] = useState([]);
+
+  useEffect(() => {
+    fetch(`/houses`)
+      .then((res) => res.json())
+      .then((houseInfo) => {
+        setHouses(houseInfo);
+      });
+  }, []);
+
+
+  if (user) {
+    return (
       <>
-      <div className="container"> 
-        <h2>Welcome, {user.username}, and find</h2>
-        <h2>yourself a house to build a home</h2>
-      </div>
+        <div className="container">
+          <h2>Welcome, {user.username}, and find</h2>
+          <h2>yourself a house to build a home</h2>
+          <Houses house={house}/>
+        </div>
       </>
-      );
-    } else {
-      return <h2>Please Login or Sign Up</h2>;
-    }
+    );
+  } else {
+    return <h2>Please Login or Sign Up</h2>;
   }
-  
-  export default Home;
-  
+}
+
+export default Home;
