27d8dfa1990f3d3bd3a726d80a9bb84f37b4333f
Bodipoh <berylodipoh@gmail.com>
2022-12-09 22:19:06 +0300

    About updated

--- client/src/Components/About/About.js
+++ client/src/Components/About/About.js
@@ -11,12 +11,11 @@ const About = () => {
           <div className='left row'>
             <h1>Our Agency Story</h1>
             <h2>Check out our company story and work process</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip.</p>
+            <p>Buying a property vs renting is a never-ending debate, but the outcome depends largely on your income and circumstances. If you are looking to move houses, and want to find a suitable rental property or shared accommodation, we can help you find the right fit.   </p>
             <button className='btn2'>More About Us</button>
           </div>
           <div className='right row'>
-            <img src='./immio.jpg' alt='' />
+            <img src='https://img.freepik.com/free-photo/house-isolated-field_1303-23772.jpg?w=826&t=st=1670613299~exp=1670613899~hmac=39a393dc74e2f5c904ad526a399c0eb0317c3edaf5a3e4d28e9e483644773f90' alt='' />
           </div>
         </div>
       </section>
