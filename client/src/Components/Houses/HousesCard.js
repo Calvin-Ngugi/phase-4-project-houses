10b83d64e8779199390d71854fb56d1c23534ba0
calvin <ncalvin67@gmail.com>
2022-12-11 00:22:48 +0300

    Added star ratings to home page

--- client/src/Components/Houses/HousesCard.js
+++ client/src/Components/Houses/HousesCard.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { BrowserRouter as Router, Link } from 'react-router-dom'
+import Rating from '../Rating'
 
 const HousesCard = ({houses}) => {
 
@@ -16,7 +17,8 @@ const HousesCard = ({houses}) => {
           <div className="card-body">
             <h4 className="card-title">{houses.name}</h4>
             <small className="text-muted">House to Let</small>
-            <div className="card-text">{houses.avg_score}</div>
+            <br/>
+            <Rating score={houses.avg_score}/>
             <p className="card-text">ksh {houses.price}/=</p>
             <div className="card-button row d-flex justify-content-between">
               <button 
