0bb0f21a0cd3997b36b524fa6523f3adc504ad26
levin-achieng-dieto <levin.dieto@student.moringaschool.com>
2022-12-11 11:01:27 +0300

    making changes

--- client/src/Components/Home.js
+++ client/src/Components/Home.js
@@ -1,33 +1,29 @@
-import { useEffect, useState } from "react";
+//import { useEffect, useState } from "react";
 import Houses from "./Houses/Houses";
-
-// const inline = {
-//   display: "flex",
-//   text: "900px"
-// }
+import React from 'react'
 
 
-function Home({ user }) {
-  const [house, setHouses] = useState([]);
+function Home({ user, houses }) {
+  // const [house, setHouses] = useState([]);
 
-  useEffect(() => {
-    fetch(`/houses`)
-      .then((res) => res.json())
-      .then((houseInfo) => {
-        setHouses(houseInfo);
-      });
-  }, []);
+  // useEffect(() => {
+  //   fetch(`/houses`)
+  //     .then((res) => res.json())
+  //     .then((houseInfo) => {
+  //       setHouses(houseInfo);
+  //     });
+  // }, []);
 
 
   if (user) {
     return (
       <>
-        <div className="container bg-green-900">
+        <div className="container">
           <div className="w-20 bg-black text-white text-center">
             <h2 className="pt-4">Welcome, {user.username}, and find</h2>
             <h2 className="pb-4">yourself a house to build a home</h2>
           </div>
-          <Houses house={house}/>
+          <Houses houses={houses}/>
         </div>
       </>
     );
@@ -35,16 +31,7 @@ function Home({ user }) {
     return (
       <>
         <div>
-          <div className="text-center">
-            <h1>Choose us Everyday!</h1>
-            <p className="pt-5 mb-20 text-4xl">OasisHomes is an exclusive chic estate of contemporary more than one </p>
-            <p className="text-5xl">bedroom apartments sitting on 12 acres at the heart of Nairobi. The cool balconies, </p>
-            <p>glass railings and floor to ceiling double-leaf doors make most of the homes’ spectacular </p>
-            <p>panoramic views to the park and the community’s expansive outdoor amenities </p>
-            <p>which are close by. Each apartment boasts of calming natural palettes and a modern kitchen </p>
-            <p className="text-5xl">giving a lovely and quality indoor experience.</p>
-            <h2 className="font-extrabold">Please Login or Sign Up</h2>
-          </div>
+          <h1>login sign up</h1>
         </div>
       </>
     );
